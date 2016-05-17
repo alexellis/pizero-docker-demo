ee6ef75fd5e17c08f93edfa87611bc3e7895af86
Alex Ellis <alexellis2@gmail.com>
2016-05-17 07:38:42 +0000

    light show

---
+++ start_lights.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+docker rm -f iot1 iot2 iot3
+
+echo "starting nodes"
+docker run --privileged -p 5000:5000 -d --label iotnode --name iot1 alexellis2/dockercon-iotnode:latest
+docker run --privileged -p 5000:5000 -d --label iotnode --name iot2 alexellis2/dockercon-iotnode:latest
+docker run --privileged -p 5000:5000 -d --label iotnode --name iot3 alexellis2/dockercon-iotnode:latest
+
+DOCKER_HOST=""
+
+docker run -e SWARM_URI=192.168.0.100:2376 -e LABEL_FILTER=iotnode -d -p 80:80 alexellis2/nginx_dynamic:v6
+
