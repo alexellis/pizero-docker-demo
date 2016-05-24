ec2f91d53db6a55708dc7947a02e12729d44bd29
Alex Ellis <alexellis2@gmail.com>
2016-05-24 22:44:59 +0000

    Add 4th node

--- start_lights.sh
+++ start_lights.sh
@@ -2,12 +2,13 @@
 
 export DOCKER_HOST=tcp://192.168.0.100:2376
 
-docker rm -f iot1 iot2 iot3
+docker rm -f iot1 iot2 iot3 iot4
 
 echo "starting nodes"
 docker run --privileged -p 5000:5000 -d --label iotnode --name iot1 alexellis2/dockercon-iotnode:latest
 docker run --privileged -p 5000:5000 -d --label iotnode --name iot2 alexellis2/dockercon-iotnode:latest
 docker run --privileged -p 5000:5000 -d --label iotnode --name iot3 alexellis2/dockercon-iotnode:latest
+docker run --privileged -p 5000:5000 -d --label iotnode --name iot4 alexellis2/dockercon-iotnode:latest
 
 DOCKER_HOST=""
 docker rm -f iotbalancer
